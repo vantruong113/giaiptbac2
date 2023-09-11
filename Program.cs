c92c7566e5d9debadffafd5d3879425384234b70
ADMIN <ADMIN@LAB1-MAY35>
2023-09-11 11:21:06 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace giaiptbac2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine ("Nhập vào a: ");
+            float a = float.Parse( Console.ReadLine() );
+
+            Console.OutputEncoding = e
+
+
+
+
+        }
+    }
+}
